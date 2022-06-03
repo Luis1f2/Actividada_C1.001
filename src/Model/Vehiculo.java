9aa18e92b2bd27d4841a8d52e272fd18d4ac746b
LUIS FERNANDO <flores123ruiz2316@gmail.com>
2022-06-03 02:45:40 -0500

    Initial commit

---
+++ src/Model/Vehiculo.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Vehiculos {
+    private int matricula;
+    private short modelo;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public short getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(short modelo) {
+        this.modelo = modelo;
+    }
+}
